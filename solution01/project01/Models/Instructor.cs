ed20ae2729844f964c3a8cb0f42eebd9620b1ded
Malakaka <malakalsenine@gmail.com>
2026-06-30 13:23:09 +0400

    update  Instructor classe

--- solution01/project01/Models/Instructor.cs
+++ solution01/project01/Models/Instructor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +10,30 @@ namespace project01.Models
 {
     public class Instructor
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int instructorId { get; set; }  // system generated
 
-    
+        [Required]
+        [MaxLength(100)]
         public string fullName { get; set; } // user input
 
-
+        [Required]
+        [MaxLength(150)]
         public string email { get; set; } // user input
 
-
+        [MaxLength(20)]
         public string officeNumber { get; set; }  // user input
 
-
+        [Required]
         public DateTime hireDate { get; set; } // user input 
 
-        public decimal salary { get; set; } // default
+        [Required]
+        
+        public decimal salary { get; set; } // user input
 
+        [Required]
+        [MaxLength(50)]
         public string academicTitle { get; set; }  // user input 
 
 
--- solution01/project01/Models/Student.cs
+++ solution01/project01/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace project01.Models
     public class Student
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int studentId { get; set; }  // system generated
 
         [Required]
