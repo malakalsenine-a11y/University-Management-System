dc0664594f2e3e665adc9d114179177d3c5e370a
Malakaka <malakalsenine@gmail.com>
2026-06-30 12:32:56 +0400

    create models folder then create Students class

---
+++ solution01/project01/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project01.Models
+{
+    public class Student
+    {
+        public int studentId { get; set; }
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public int enrollmentYear { get; set; }
+        public decimal gpa { get; set; }
+
+
+    }
+}
--- solution01/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
+++ solution01/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9844b1fa4ada02846a8d427572b6b33271d66daf")]
 [assembly: System.Reflection.AssemblyProductAttribute("project01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
