a2dc85c292224fe7271233d3d3dd9c4fe89dcca5
Malakaka <malakalsenine@gmail.com>
2026-06-30 13:05:00 +0400

    add The appropriate data annotation Students class

--- solution01/project01/Models/Student.cs
+++ solution01/project01/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,29 @@ namespace project01.Models
 {
     public class Student
     {
-        public int studentId { get; set; }
-        public string fullName { get; set; }
-        public string email { get; set; }
-        public string phoneNumber { get; set; }
-        public DateTime dateOfBirth { get; set; }
-        public int enrollmentYear { get; set; }
-        public decimal gpa { get; set; }
+        [Key]
+        public int studentId { get; set; }  // system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string fullName { get; set; } // user input
+
+        [Required]
+        [MaxLength(150)]
+        public string email { get; set; } // user input
+
+        [MaxLength(20)]
+        public string phoneNumber { get; set; }  // user input
+
+        [Required]
+        public DateTime dateOfBirth { get; set; } // user input 
+
+        [Required]
+        [Range(2000, 2030)]
+        public int enrollmentYear { get; set; }  // user input 
+
+        [Range(0.0, 4.0)]
+        public decimal gpa { get; set; } // default
 
 
     }
